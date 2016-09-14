cc0fd55d44e644400e806e387d698eb22c3275ab
daxapps <daxapps777@gmail.com>
2016-09-14 16:38:51 -0500

    sec1.4

--- MyCodingClickCounter/ViewController.swift
+++ MyCodingClickCounter/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    var label:UILabel!
+    var label2:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Label
+        let label = UILabel()
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+        self.label = label
+        
+        let label2 = UILabel()
+        label2.frame = CGRectMake(200, 150, 60, 60)
+        label2.text = "0"
+        
+        self.view.addSubview(label2)
+        self.label2 = label2
+        
+        //Button
+        let button = UIButton()
+        button.frame = CGRectMake(150, 250, 60, 60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), forControlEvents: UIControlEvents.TouchUpInside)
+        button.addTarget(self, action: #selector(ViewController.changeBackground), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        let button2 = UIButton()
+        button2.frame = CGRectMake(200, 250, 60, 60)
+        button2.setTitle("Click", forState: .Normal)
+        button2.setTitleColor(UIColor.redColor(), forState: .Normal)
+        self.view.addSubview(button2)
+        
+        button2.addTarget(self, action: #selector(ViewController.decrementCount), forControlEvents: UIControlEvents.TouchUpInside)
+        button2.addTarget(self, action: #selector(ViewController.changeBackground2), forControlEvents: UIControlEvents.TouchUpInside)
+        
+    }
+    
+    func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func decrementCount() {
+        self.count -= 1
+        self.label2.text = "\(self.count)"
+        
     }
-
+    
+    func changeBackground() {
+        self.view.layer.backgroundColor = UIColor.cyanColor().CGColor
+    }
+    
+    func changeBackground2() {
+        self.view.layer.backgroundColor = UIColor.magentaColor().CGColor
+    }
 
 }
 
